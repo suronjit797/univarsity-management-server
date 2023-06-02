36bb54da2492e6718627ada71a6eaf2920366010
suronjit797 <suronjit797@gmail.com>
2023-06-03 00:30:47 +0600

    husky

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,14 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
-
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 
 const app: Application = express()
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+            app.use(express.json())
+            app.use(express.urlencoded({ extended: true }))
+            app.use(cors())
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('<h1> Welcome to PH University </h1>');
+  res.send('<h1> Welcome to PH University </h1>')
 })
-
-
 
 export default app
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,10 @@
-import dotenv from "dotenv";
-import path from "path";
+import dotenv from 'dotenv'
+import path from 'path'
 
-dotenv.config({path: path.join(process.cwd(), '.env')})
-
+dotenv.config({ path: path.join(process.cwd(), '.env') })
 
 export default {
-    PORT: process.env.PORT,
-    DB_URI: process.env.DB_URI,
-    NODE_ENV: process.env.NODE_ENV
+  PORT: process.env.PORT,
+  DB_URI: process.env.DB_URI,
+  NODE_ENV: process.env.NODE_ENV,
 }
