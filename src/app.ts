b956d6e42bc22aa064cc4318f429d6eb06dbf548
suronjit797 <suronjit797@gmail.com>
2023-06-03 00:33:15 +0600

    husky

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,10 @@ import cors from 'cors'
 
 const app: Application = express()
 
-      app.use(express.json())
-      app.use(express.urlencoded({ extended: true }))
-      app.use(cors())
-      
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+
 app.get('/', (req: Request, res: Response) => {
   res.send('<h1> Welcome to PH University </h1>')
 })
